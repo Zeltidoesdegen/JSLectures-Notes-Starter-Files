6e7cfc8e788ccabe9037a85c66b446e68bb9409b
Zeltidoesdegen <ivan.carsula@gmail.com>
2023-08-23 13:18:24 +0800

    Notes & variable manipulation

--- 02-setinterval-clearinterval/script.js
+++ 02-setinterval-clearinterval/script.js
@@ -2,10 +2,35 @@
 //     console.log("Hell");
 // }, 2000);
 
+var interval;
+
 var changeColor = () => {
-    setInterval(() => {
-        console.log("Hello");
-    }, 2000)
+// 1st approach
+// if(interval) {
+
+
+// }else{
+//     interval = setInterval(() => {
+//         const color = Math.floor(Math.random()* 16777215).toString(16);
+//         document.body.style.backgroundColor = `#${color}`
+//     }, 1000)
+// }
+// }
+
+// 2nd approach
+    if(!interval){
+        interval = setInterval(() => {
+            const color = Math.floor(Math.random()* 16777215).toString(16);
+            document.body.style.backgroundColor = `#${color}`
+            console.log(color);
+        }, 1000)
+    }
+
 }
 
-document.querySelector("#start").addEventListener("click", changeColor)
+let stopColor = () => {
+    clearInterval(interval);
+}
+
+document.querySelector("#start").addEventListener("click", changeColor);
+document.querySelector("#stop").addEventListener("click", stopColor);
